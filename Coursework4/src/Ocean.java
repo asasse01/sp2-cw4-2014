957cfc998f48755ec76d1d22ada2a855c0718fed
= <sassela@live.com>
2015-01-06 12:51:33 +0000

    Add placeAllShipsRandomly method

--- Coursework4/src/Ocean.java
+++ Coursework4/src/Ocean.java
@@ -1,3 +1,5 @@
+import java.util.Random;
+
 /**
  * Created by abby on 22/12/14.
  */
@@ -6,6 +8,9 @@ public class Ocean {
     int shotsFired;
     int hitCount;
     int shipsSunk;
+    final int MAX = ships.length; // ocean border. Assumes board is square
+    Random rand = new Random();
+
 
     public Ocean() {
         shotsFired = 0;
@@ -13,8 +18,8 @@ public class Ocean {
         shipsSunk = 0;
         EmptySea e = new EmptySea();
 
-        for(int i = 0; i < ships.length; i++) {
-            for(int j = 0; j < ships.length; j++) {
+        for(int i = 0; i < MAX; i++) {
+            for(int j = 0; j < MAX; j++) {
                 ships[i][j] = e;
             }
         }
@@ -22,11 +27,43 @@ public class Ocean {
     }
 
     void placeAllShipsRandomly() {
-        // TODO write method
+		int row;
+		int column;
+		boolean horizontal;
+
+		
+    	Ship[] fleet = new Ship[10];
+    	fleet[0] = new Battleship();
+    	fleet[1] = new Cruiser();
+    	fleet[2] = new Cruiser();
+    	fleet[3] = new Destroyer();
+    	fleet[4] = new Destroyer();
+    	fleet[5] = new Destroyer();
+    	fleet[6] = new Submarine();
+    	fleet[7] = new Submarine();
+    	fleet[8] = new Submarine();
+    	fleet[9] = new Submarine();
+
+    	for(Ship ship : fleet) {
+    		horizontal = rand.nextBoolean();
+    		
+    		do {
+    			row = rand.nextInt(MAX + 1);
+    			column = rand.nextInt(MAX + 1);
+    			
+    		} while (!(ship.okToPlaceShipAt(row, column, horizontal, this)));
+    		
+    		ship.placeShipAt(row, column, horizontal, this);
+    	}
     }
 
     boolean isOccupied(int row, int column) {
-        // TODO write method
+    	boolean occupied = false;
+    	if (!(ships[row][column] instanceof EmptySea)) {
+    		occupied = true;
+    	}
+    	
+    	return occupied;
     }
 
     boolean shootAt(int row, int column) {
