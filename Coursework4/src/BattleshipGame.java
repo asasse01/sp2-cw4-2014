204bf1e04da8a487ab221ff0ae8be911bb14bb8d
= <sassela@live.com>
2015-01-08 07:31:43 +0000

    Correct BattleshipGame to match coursework spec

--- Coursework4/src/BattleshipGame.java
+++ Coursework4/src/BattleshipGame.java
@@ -3,63 +3,62 @@
  */
  
  import java.util.Scanner;
-public class BattleshipGame {
+ public class BattleshipGame {
 
-    public static void main(String[] args) {
-        
-        Scanner in = new Scanner(System.in);
-        
-        runGame();
-        requestReplay();
-        
-        in.close();
-       
-    }
-    
-    void runGame() {
+
+    public static void main(String[] args) {        
         
         Ocean ocean = new Ocean();
         ocean.placeAllShipsRandomly();
+        
+        runGame(ocean);
+        requestReplay(ocean);
+        
+       
+    }
+    
+    static void runGame(Ocean ocean) {
+    	Scanner in = new Scanner(System.in);
         
         boolean shot = false;
         int x;
         int y;
         
          System.out.println("Welcome to the Battleship Game.");
+         ocean.print();
         
         do {
+            
             System.out.println("Please enter your shots, giving coordinates x and y on a new line");
             x = in.nextInt();
             y = in.nextInt();
             
-            ocean.print();
             shot = ocean.shootAt(x, y);
             if(shot){
                 if(ocean.ships[x][y].isSunk()){// TODO create Ocean method
-                    System.out.println("You just sunk a "+ocean.ships[x][y].getShipType());
+                    System.out.println("You just sunk a "+ ocean.ships[x][y].getShipType());
                 } else System.out.println("Hit");
             } else System.out.println("Miss");
-            ocean.print();
-
-        } while !(isGameOver());
+            ocean.print();    
+        } while (!(ocean.isGameOver()));
         
-        System.out.println("Game completed. Final Scores:");
-        System.out.println("Shots fired: " + ocean.getShotsFired();
-        System.out.println("Hits: " + ocean.getHitCount();
-        System.out.println("Ships sunk: " + ocean.getShipsSunk();
+        System.out.println("Game completed. Final Score (shots fired): " + ocean.getShotsFired());
         
+        in.close();
     }
     
-    void requestReplay() {
+    static void requestReplay(Ocean ocean) {
+    	Scanner in = new Scanner(System.in);
+    	
         String replayGame;
         boolean validResponse = true;
 
         do {
             System.out.println("Would you like to play again? Y/N");
-            replayGame = in.Next();
+            replayGame = in.next();
             if(replayGame == "Y") { //switch statement
                 validResponse = true;
-                runGame(); //move method
+                runGame(ocean);
             } else if(replayGame == "N") {
                 validResponse = true;
                 break;
@@ -68,5 +67,7 @@ public class BattleshipGame {
                 validResponse = false;
             }
         } while (!(validResponse));
+        
+        in.close();
     }
 }
